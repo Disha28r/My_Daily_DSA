90943dd423147d18eabf1dd793bfbfbbca3f9e2e
Disha R <125009668+Disha28r@users.noreply.github.com>
2026-09-09 00:33:08 +0530

    Time: 1 ms (70.01%) | Memory: 21.8 MB (10.69%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+
+    def singleNumber(self, nums: List[int]) -> int:
+        dic=Counter(nums)
+        for key,value in dic.items():
+            if value==1:
+                return key
+        
